30ab51206259bf977f188e85a4c027fad8b0eca9
Nguyen Van An <nguyenvanan9a4@gmail.com>
2023-03-11 21:56:12 +0700

    AC_Studio

---
+++ js/login.js
@@ -0,0 +1,13 @@
+<script>
+  	function login(){
+var username = document.getElementById("inputEmail").value;
+var password = document.getElementById("inputPassword").value;
+if ( username == "nguyenvanan19it1@gmail.com" && password == "0906554075"){
+alert ("Login successfully");
+     }
+     else{
+       alert("Invalid username or password");
+       }
+     return false;
+     }
+  </script>
